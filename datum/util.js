5d9c4329c55c54470ba9369ce770036e9a8fb028
unknown <865553742@qq.com>
2019-08-12 09:08:38 +0800

    feat: 32 题

--- datum/util.js
+++ datum/util.js
@@ -1,8 +1,12 @@
 const encodeTitle = (number, text) => {
   const title = `第 ${number} 题: ${text}`;
-  console.log(`https://github.com/spaasteam/spaas-daily-practice/issues/new?title=${encodeURIComponent(title)}`)
-  console.log(`### ${title}`)
-  return `* [\`${title}\`](#${encodeURI(title)})`
-}
+  console.log(
+    `https://github.com/spaasteam/spaas-daily-practice/issues/new?title=${encodeURIComponent(
+      title
+    )}`
+  );
+  console.log(`### ${title}`);
+  return `* [\`${title}\`](#${encodeURI(title)})`;
+};
 
-console.log(encodeTitle(31, "单词搜索"));
+console.log(encodeTitle(32, "格式化 money"));
